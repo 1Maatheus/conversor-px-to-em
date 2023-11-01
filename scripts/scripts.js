bb7f95ce87a2842bec5b01f8cba52aaa85420137
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2023-11-01 01:33:15 -0300

    adding first template idea

---
+++ scripts/scripts.js
@@ -0,0 +1,11 @@
+const input = document.querySelector(".input");
+const result = document.querySelector(".main-result");
+const btnConvertor = document.getElementById("btn-convertor");
+
+function pxConvertor() {
+  let em = 1;
+  let px = 16;
+
+  result.textContent = input.value / px;
+}
+input.addEventListener("input", pxConvertor);
--- src/script.js
+++
@@ -1,9 +0,0 @@
-// const input = document.querySelector(".input");
-// const div = document.querySelector(".resposta");
-
-// input.addEventListener("input", () => {
-//   let em = 1;
-//   let px = 16;
-
-//   div.textContent = input.value / px;
-// });
